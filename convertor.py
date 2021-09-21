4dbb9394eab43f6049374fae881fb0fa3481deb2
hooman.the110 <hooman.the110@gmail.com>
2021-09-21 17:59:22 +0330

    add comment

--- convertor.py
+++ convertor.py
@@ -1,4 +1,8 @@
 def str_and_bytes_convertor(chardata, operation = "byte_to_str", u_encoding = "utf-8"):
+    '''
+    this function check and converts string and bytes data type to each other
+    also can accept encoding as argument 
+    '''
     result = chardata
     if(operation == "byte_to_str"):
         if isinstance(chardata, bytes):
@@ -8,9 +12,3 @@ def str_and_bytes_convertor(chardata, operation = "byte_to_str", u_encoding = "u
             result = chardata.encode(u_encoding) 
     return result
 
-byte = b'h\x65llo'
-string = 'a\u0300 propos'
-out_string = str_and_bytes_convertor(byte)
-out_byte = str_and_bytes_convertor(string, 'str_to_byte')
-print(type(string))
-print(type(out_byte))
